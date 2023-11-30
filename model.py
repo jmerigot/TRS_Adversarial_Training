bb27f304e29ef85d5638b4b92e05c95784186f76
jmerigot <julesmerigot@gmail.com>
2023-11-30 15:18:17 +0100

    decreased final epsilon val

--- model.py
+++ model.py
@@ -110,7 +110,7 @@ def train_model_adversarial(net, train_loader, pth_filename, num_epochs, eps=0.0
     criterion = nn.NLLLoss()
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
     
-    final_eps = 0.5
+    final_eps = 0.2
     final_alpha = 0.03
 
     for epoch in tqdm(range(num_epochs)):  
