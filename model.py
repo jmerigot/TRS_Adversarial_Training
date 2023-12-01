2db72ff194e2c6f39f22d153d22f61b3c445b9e2
jmerigot <julesmerigot@gmail.com>
2023-12-01 11:20:49 +0100

    increase lr to 0.05 for test

--- model.py
+++ model.py
@@ -108,7 +108,7 @@ def update_eps_alpha(epoch, num_epochs, eps, final_eps, alpha):
 def train_model_adversarial(net, train_loader, pth_filename, num_epochs, eps=0.03, alpha=0.01, iters=40, step_size=1, gamma=0.75):
     print("Starting training with adversarial examples")
     criterion = nn.NLLLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
+    optimizer = optim.SGD(net.parameters(), lr=0.05, momentum=0.9)
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)
     
     final_eps = 0.08
@@ -135,8 +135,9 @@ def train_model_adversarial(net, train_loader, pth_filename, num_epochs, eps=0.0
 
             # print statistics
             running_loss += loss.item()
-            if i % 500 == 499:
-                print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 2000))
+            if i % 500 == 499:      # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' % 
+                      (epoch + 1, i + 1, running_loss / 2000))
                 running_loss = 0.0
                 
         scheduler.step()
