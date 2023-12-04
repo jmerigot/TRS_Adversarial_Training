32288b705e1067cb08ef1009940bd62ca0df754d
jmerigot <julesmerigot@gmail.com>
2023-12-04 17:53:03 +0100

    another debug

--- model.py
+++ model.py
@@ -184,10 +184,9 @@ def pgd_attack(model, images, labels, eps, alpha, iters):
     return images
 
 def pgd_attack_l2(model, images, labels, eps, alpha, iters):
-    print(device)
     original_images = images.clone().detach()#.to(device)
     images = images.clone().detach().to(device)
-    labels = labels.clone().detach().to(device)
+    #labels = labels.clone().detach().to(device)
 
     for _ in range(iters):
         images.requires_grad = True
